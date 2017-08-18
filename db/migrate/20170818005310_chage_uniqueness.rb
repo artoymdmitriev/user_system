4d0b76fabf3058557329007ac7a36d6eb8aae3f3
artyom <admitriev5813@gmail.com>
2017-08-18 04:18:47 +0300

    Add full authorization features

--- db/migrate/20170818005310_chage_uniqueness.rb
+++ db/migrate/20170818005310_chage_uniqueness.rb
@@ -1,5 +1,6 @@
 class ChageUniqueness < ActiveRecord::Migration[5.0]
   def change
   	add_index :users, [:email, :provider], unique: true
+  	remove_index(:users, :name => 'index_users_on_email')
   end
 end
